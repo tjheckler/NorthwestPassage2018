6a0aeff3ba0dc055223c129c36c4c344242c8874
ManBearSquirrel <lrflrf@sbcglobal.net>
2018-05-21 15:16:23 -0500

    Update GameController.java

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -3,25 +3,6 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-//BRONZE
-
-//SILVER
-
-//GOLD
-//TODO Make the western route have a "Meeting with Santa" page that
-//TODO   the player has a 50% chance of seeing
-//TODO On each screen show the list of able bodied crew and announce
-//TODO   the name of the injured crew person
-//TODO Consolidate the end game pages into a single page to reduce duplicate code
-//TODO Add an Easter Egg (Google or use search engine of your choice for "Easter Egg Game"
-//TODO   if an explanation is needed)
-
-//PLATINUM
-//TODO On the welcome screen allow the user to pick one of four pictures of a kitten
-//TODO   to represent themselves.  Show this image on each screen of the game.
-//TODO Implement a 20% chance that the fidget spinners are covered in lead paint
-//TODO   When this happens the Queen should throw the player in prison
-
 public class GameController extends Controller
 {
     public Result getWelcome()
