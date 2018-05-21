70fcef5d6cd00ef564bce7fcee96b02258374fc1
tjheckler <timothyheckler@gmail.com>
2018-05-21 16:56:30 -0500

    Bronze complete

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,10 +1,22 @@
 package controllers;
 
+import play.api.mvc.Session;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+
 public class GameController extends Controller
 {
+    private FormFactory formFactory;
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory=formFactory;
+    }
     public Result getWelcome()
     {
         return ok(views.html.welcome.render());
@@ -12,6 +24,10 @@ public class GameController extends Controller
 
     public Result postStart()
     {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        String name = form.get("name");
+        session().put("name",name);
+
         return ok(views.html.start.render());
     }
 
@@ -22,22 +38,28 @@ public class GameController extends Controller
 
     public Result postNorthFromEngland()
     {
-        return ok(views.html.northfromengland.render());
+        String name = session().get("name");
+        return ok(views.html.northfromengland.render(name));
     }
 
     public Result postWestFromEngland()
     {
         return ok(views.html.westfromengland.render());
     }
+    public Result postBetweenNorthAndStart(){
+        return ok(views.html.betweenNorthAndStart.render());
+    }
 
     public Result postEastEnd()
     {
-        return ok(views.html.eastend.render());
+        String name = session().get("name");
+        return ok(views.html.eastend.render(name));
     }
 
     public Result postWestEnd()
     {
-        return ok(views.html.westend.render());
+        String name = session().get("name");
+        return ok(views.html.westend.render(name));
     }
 
     public Result postHomePort()
